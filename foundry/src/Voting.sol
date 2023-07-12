93cbe067fe41268de4c947a239880a43493284bb
abishegsri <137195570+abishegsri@users.noreply.github.com>
2023-07-12 17:34:41 +0530

    Update Voting.sol

--- foundry/src/Voting.sol
+++ foundry/src/Voting.sol
@@ -1,61 +1,60 @@
+pragma solidity ^0.8.14;
+import "@openzeppelin/contracts/access/Ownable.sol";
 contract Voting is Ownable
 {
-    enum Stage{vote,result}
-    Stage public stage=Stage.vote;
-    struct Candidate
+    mapping (address=>bytes32) Voterscommit;
+    mapping (string=>uint) votes;
+        struct Candidate
     {
         string name;
     }
-    mapping (bytes32=>string) hashtoname;
-    function returnHash(string memory name) public returns (bytes32)
-    {
-        hashtoname[keccak256(bytes(name))]=name;
-        return keccak256(bytes(name));
-    }
-    mapping (uint => uint) noofVotes;
-    mapping(string => uint )nametoid;
     Candidate [] candidates;
     constructor()
     {
         //list of predefined candidates as mentioned in the problem statement
         candidates.push(Candidate("mark"));
-        nametoid["mark"]=1;
         candidates.push(Candidate("john"));
-        nametoid["john"]=2;
         candidates.push(Candidate("dev"));
-        nametoid["dev"]=3;
         candidates.push(Candidate("sundar"));
-        nametoid["sundar"]=4;
     }
-    mapping (address => bool) public addvoted;
-    function vote(bytes32 candi) public {
-        require(stage==Stage.vote,"The Voting Process has been Completed");
+    enum Stage{commit,reveal}
+    Stage stage=Stage.commit;
+    mapping (address => bool)addvoted;
+    function vote_commit(bytes32 cand) public 
+    {
+        require(stage==Stage.commit,"Voting is not completed");
         require(addvoted[msg.sender]==false,"Voter can vote only once");
         addvoted[msg.sender]=true;
-        noofVotes[nametoid[hashtoname[candi]]]++;
+        Voterscommit[msg.sender]=cand;
     }
-    function getVoteCount(uint _candidateId) view internal  returns(uint) 
+    function revealVote(string memory name,string memory salt) public
     {
-        return noofVotes[_candidateId];
+        require(stage==Stage.reveal);
+        require(keccak256(abi.encodePacked(name,salt))==Voterscommit[msg.sender]);
+        votes[name]++;
+    }
+    function delecareWinner() onlyOwner public view returns(string memory)
+    {
+        require(stage==Stage.reveal,"Voting has not completed");
+        uint length=candidates.length;
+        uint maxVotes;
+        string memory result;
+        for(uint i=0;i<length;i++)
+        {
+           if(votes[candidates[i].name]>maxVotes)
+           {
+               maxVotes=votes[candidates[i].name];
+               result=candidates[i].name;
+           }
+        }
+        return result;
+    }
+    function giveHash(string memory name,string memory salt) public pure returns(bytes32)
+    {
+        return keccak256(abi.encodePacked(name,salt));
     }
     function completeVoting() public onlyOwner
     {
-        stage=Stage.result;
-    }
-    function delecareWinner() onlyOwner public view returns(uint)
-    {
-        require(stage==Stage.result,"Voting has not completed");
-        uint length=candidates.length;
-        uint maxVotes;
-        uint result;
-        for(uint i=0;i<length;i++)
-        {
-            if(getVoteCount(i+1)>maxVotes)
-            {
-                maxVotes=getVoteCount(i+1);
-                result=i+1;
-            }
-        }
-        return result;
+        stage=Stage.reveal;
     }
 }
