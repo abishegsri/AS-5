bfe30f94f38884e65018ec7bbd950182ffb9711a
abishegsri <137195570+abishegsri@users.noreply.github.com>
2023-07-12 20:47:38 +0530

    Update Voting.sol

--- foundry/src/Voting.sol
+++ foundry/src/Voting.sol
@@ -2,36 +2,41 @@ pragma solidity ^0.8.14;
 import "@openzeppelin/contracts/access/Ownable.sol";
 contract Voting is Ownable
 {
-    mapping (address=>bytes32) Voterscommit;
-    mapping (string=>uint) votes;
+    mapping (address=>bytes32) voterCommit;
+    mapping (string=>uint) voteCasted;
         struct Candidate
     {
-        string name;
+        string Candidatename;
     }
     Candidate [] candidates;
-    constructor()
+    constructor(string[] memory _candidatelist)
     {
         //list of predefined candidates as mentioned in the problem statement
-        candidates.push(Candidate("mark"));
-        candidates.push(Candidate("john"));
-        candidates.push(Candidate("dev"));
-        candidates.push(Candidate("sundar"));
+        for(uint i=0;i<_candidatelist.length;i++)
+        {
+            candidates.push(Candidate(_candidatelist[i]));
+        }
     }
+    event commitCreated(address _sender,bytes32 _commitmade);
+    event revealMade(string _name,string  _salt,address _sender);
+    event votingCompleted();
     enum Stage{commit,reveal}
     Stage stage=Stage.commit;
-    mapping (address => bool)addvoted;
+    mapping (address => bool) _isaddressVoted;
     function vote_commit(bytes32 cand) public 
     {
         require(stage==Stage.commit,"Voting is not completed");
-        require(addvoted[msg.sender]==false,"Voter can vote only once");
-        addvoted[msg.sender]=true;
-        Voterscommit[msg.sender]=cand;
+        require(_isaddressVoted[msg.sender]==false,"Voter can vote only once");
+        _isaddressVoted[msg.sender]=true;
+        voterCommit[msg.sender]=cand;
+        emit commitCreated(msg.sender,cand);
     }
     function revealVote(string memory name,string memory salt) public
     {
         require(stage==Stage.reveal);
-        require(keccak256(abi.encodePacked(name,salt))==Voterscommit[msg.sender]);
-        votes[name]++;
+        require(keccak256(abi.encodePacked(name,salt))==voterCommit[msg.sender]);
+        voteCasted[name]++;
+        emit revealMade(name, salt, msg.sender);
     }
     function delecareWinner() onlyOwner public view returns(string memory)
     {
@@ -41,10 +46,10 @@ contract Voting is Ownable
         string memory result;
         for(uint i=0;i<length;i++)
         {
-           if(votes[candidates[i].name]>maxVotes)
+           if(voteCasted[candidates[i].Candidatename]>maxVotes)
            {
-               maxVotes=votes[candidates[i].name];
-               result=candidates[i].name;
+               maxVotes=voteCasted[candidates[i].Candidatename];
+               result=candidates[i].Candidatename;
            }
         }
         return result;
@@ -56,5 +61,6 @@ contract Voting is Ownable
     function completeVoting() public onlyOwner
     {
         stage=Stage.reveal;
+        emit votingCompleted();
     }
 }
